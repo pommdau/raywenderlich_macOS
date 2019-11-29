4fd2c4edc59004f938a0448e47edb2de61623a32
hikeuchi <hikeuchi@morisawa.co.jp>
2019-11-29 11:34:42 +0900

    メニューアイテムの整理と追加

--- 01_macOS Development for Beginners/EggTimer/EggTimer/AppDelegate.swift
+++ 01_macOS Development for Beginners/EggTimer/EggTimer/AppDelegate.swift
@@ -11,7 +11,9 @@ import Cocoa
 @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate {
 
-
+    @IBOutlet weak var startTimerMenuItem: NSMenuItem!
+    @IBOutlet weak var stopTimerMenuItem: NSMenuItem!
+    @IBOutlet weak var resetTimerMenuItem: NSMenuItem!
 
     func applicationDidFinishLaunching(_ aNotification: Notification) {
         // Insert code here to initialize your application
--- 01_macOS Development for Beginners/EggTimer/EggTimer/ViewController.swift
+++ 01_macOS Development for Beginners/EggTimer/EggTimer/ViewController.swift
@@ -37,5 +37,20 @@ class ViewController: NSViewController {
     @IBAction func resetButtonClicked(_ sender: Any) {
     }
     
+    
+    // MARK: - IBActions - menus
+    
+    @IBAction func startTimerMenuItemSelected(_ sender: Any) {
+        startButtonClicked(sender)
+    }
+    
+    @IBAction func stopTimerMenuItemSelected(_ sender: Any) {
+        stopButtonClicked(sender)
+    }
+    
+    @IBAction func resetTimerMenuItemSelected(_ sender: Any) {
+        resetButtonClicked(sender)
+    }
+    
 }
 
