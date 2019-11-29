de9ddf4726dcf83e627472cd79b59189aee6dc2f
hikeuchi <hikeuchi@morisawa.co.jp>
2019-11-29 09:53:08 +0900

    initial commit

---
+++ 01_macOS Development for Beginners/HelloWorld/HelloWorld/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  HelloWorld
+//
+//  Created by HIROKI IKEUCHI on 2019/11/29.
+//  Copyright © 2019年 hikeuchi. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
