9d6e4f8f49db7f3046736900694fbc463234043e
hikeuchi <hikeuchi@morisawa.co.jp>
2019-11-29 10:52:30 +0900

    ボタンの配置

---
+++ 01_macOS Development for Beginners/EggTimer/EggTimer/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  EggTimer
+//
+//  Created by HIROKI IKEUCHI on 2019/11/29.
+//  Copyright © 2019年 hikeuchi. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
