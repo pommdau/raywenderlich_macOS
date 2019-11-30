9356d01d1499b27e317de323ac2e2e3d1d425392
IKEH <pondau1024@gmail.com>
2019-11-30 17:01:41 +0900

    create project

---
+++ 01_macOS Development for Beginners/PomodoroTimer/PomodoroTimer/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  PomodoroTimer
+//
+//  Created by Hiroki Ikeuchi on 2019/11/30.
+//  Copyright © 2019 ikeh1024. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ 01_macOS Development for Beginners/PomodoroTimer/PomodoroTimer/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  PomodoroTimer
+//
+//  Created by Hiroki Ikeuchi on 2019/11/30.
+//  Copyright © 2019 ikeh1024. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
