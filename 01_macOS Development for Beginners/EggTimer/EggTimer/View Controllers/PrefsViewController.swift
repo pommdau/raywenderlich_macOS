42a6f147383150115d20290e6d48cbe1680b7af0
IKEH <pondau1024@gmail.com>
2019-11-30 15:53:14 +0900

    設定画面のデータモデルを実装

--- 01_macOS Development for Beginners/EggTimer/EggTimer/View Controllers/PrefsViewController.swift
+++ 01_macOS Development for Beginners/EggTimer/EggTimer/View Controllers/PrefsViewController.swift
@@ -14,21 +14,73 @@ class PrefsViewController: NSViewController {
     @IBOutlet weak var customSlider: NSSlider!
     @IBOutlet weak var customTextField: NSTextField!
     
+    var prefs = Preferences()   // 設定画面用のデータ
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do view setup here.
+        showExistingPrefs()
     }
     
-    @IBAction func popupValueChanged(_ sender: Any) {
+    // 1
+    @IBAction func popupValueChanged(_ sender: NSPopUpButton) {
+        if sender.selectedItem?.title == "Custom" {
+            customSlider.isEnabled = true
+            return
+        }
+        
+        let newTimerDuration = sender.selectedTag()
+        customSlider.integerValue = newTimerDuration
+        showSliderValueAsText()
+        customSlider.isEnabled = false
     }
     
-    @IBAction func sliderValueChanged(_ sender: Any) {
+    // 2
+    @IBAction func sliderValueChanged(_ sender: NSSlider) {
+        showSliderValueAsText()
     }
     
+    // 3
     @IBAction func cancelButtonClicked(_ sender: Any) {
+        view.window?.close()
     }
     
+    // 4
     @IBAction func okButtonClicked(_ sender: Any) {
+        saveNewPrefs()
+        view.window?.close()
+    }
+
+    
+    func showExistingPrefs() {
+        let selectedTimeInMinutes = Int(prefs.selectedTime) / 60
+        
+        // とりあえずカスタム想定で、カスタムじゃなかったら下記で変更
+        presetsPopup.selectItem(withTitle: "Custom")
+        customSlider.isEnabled = true
+        
+        for item in presetsPopup.itemArray {
+            if item.tag == selectedTimeInMinutes {
+                presetsPopup.select(item)
+                customSlider.isEnabled = false
+                break
+            }
+        }
+        
+        customSlider.integerValue = selectedTimeInMinutes   // スライダに値をセット
+        showSliderValueAsText()
+    }
+    
+    func showSliderValueAsText() {
+        let newTimerDuration = customSlider.integerValue
+        let minutesDescription = (newTimerDuration == 1) ? "minute" : "minutes"
+        customTextField.stringValue = "\(newTimerDuration) \(minutesDescription)"
+    }
+
+    func saveNewPrefs() {
+        prefs.selectedTime = customSlider.doubleValue * 60
+        NotificationCenter.default.post(name: Notification.Name(rawValue: "PrefsChanged"),
+                                        object: nil)
     }
     
 }
