92e5cc4502b056173fcf0c95ac7afe0cf6325b75
hikeuchi <hikeuchi@morisawa.co.jp>
2019-12-03 11:57:23 +0900

    設定パネルのUI追加

---
+++ 01_macOS Development for Beginners/PomodoroTimer/PomodoroTimer/PrefsViewController.swift
@@ -0,0 +1,18 @@
+//
+//  PrefsViewController.swift
+//  PomodoroTimer
+//
+//  Created by HIROKI IKEUCHI on 2019/12/03.
+//  Copyright © 2019年 ikeh1024. All rights reserved.
+//
+
+import Cocoa
+
+class PrefsViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do view setup here.
+    }
+    
+}
