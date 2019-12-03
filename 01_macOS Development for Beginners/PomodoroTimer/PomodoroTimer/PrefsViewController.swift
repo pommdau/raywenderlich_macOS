254a1ba337ff83222e1f629d0c7e0dc6554f0a03
hikeuchi <hikeuchi@morisawa.co.jp>
2019-12-03 13:10:42 +0900

    設定ウィンドウのガワを作成

--- 01_macOS Development for Beginners/PomodoroTimer/PomodoroTimer/PrefsViewController.swift
+++ 01_macOS Development for Beginners/PomodoroTimer/PomodoroTimer/PrefsViewController.swift
@@ -10,9 +10,34 @@ import Cocoa
 
 class PrefsViewController: NSViewController {
 
+    @IBOutlet weak var taskLabel: NSTextField!
+    @IBOutlet weak var taskSlider: NSSlider!
+    @IBOutlet weak var intervalLabel: NSTextField!
+    @IBOutlet weak var intervalSlider: NSSlider!
+    @IBOutlet weak var longIntervalLabel: NSTextField!
+    @IBOutlet weak var longIntervalSlider: NSSlider!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do view setup here.
+    }
+    
+    
+    // MARK:- IBAction Methods
+    
+    @IBAction func taskSliderValueChanged(_ sender: NSSlider) {
+    }
+    
+    @IBAction func intervalSliderValueChanged(_ sender: NSSlider) {
+    }
+    
+    @IBAction func longIntervalSliderValueChanged(_ sender: NSSlider) {
+    }
+    
+    @IBAction func okButtonClicked(_ sender: Any) {
+    }
+    
+    @IBAction func cancelButtonClicked(_ sender: Any) {
     }
     
 }
