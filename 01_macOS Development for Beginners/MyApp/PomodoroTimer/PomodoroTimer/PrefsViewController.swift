506e00cc4083bc6f01f96e13d4a48299f5438c4a
hikeuchi <hikeuchi@morisawa.co.jp>
2019-12-09 17:23:04 +0900

    PreferencesWindowの追加

---
+++ 01_macOS Development for Beginners/MyApp/PomodoroTimer/PomodoroTimer/PrefsViewController.swift
@@ -0,0 +1,18 @@
+//
+//  PrefsViewController.swift
+//  PomodoroTimer
+//
+//  Created by HIROKI IKEUCHI on 2019/12/09.
+//  Copyright © 2019年 hikeuchi. All rights reserved.
+//
+
+import Cocoa
+
+class PrefsViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do view setup here.
+    }
+    
+}
