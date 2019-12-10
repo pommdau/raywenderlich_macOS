45f54fca2821005998012320e4c7c99a31e9d6d6
IKEH <pondau1024@gmail.com>
2019-12-10 21:37:32 +0900

    設定画面の設定値を保存する実装

--- 01_macOS Development for Beginners/MyApp/PomodoroTimer/PomodoroTimer/View Controllers/PrefsViewController.swift
+++ 01_macOS Development for Beginners/MyApp/PomodoroTimer/PomodoroTimer/View Controllers/PrefsViewController.swift
@@ -10,19 +10,45 @@ import Cocoa
 
 class PrefsViewController: NSViewController {
 
-    @IBOutlet weak var tastTimeTextField: NSTextField!
-    @IBOutlet weak var intervalTimeTextField: NSTextField!
-    @IBOutlet weak var longIntervalTimeTextField: NSTextField!
+    @IBOutlet weak var taskDurationTextField         : NSTextField!
+    @IBOutlet weak var intervalDurationTextField     : NSTextField!
+    @IBOutlet weak var longIntervalDurationTextField : NSTextField!
+    
+    var prefs = Preferences()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do view setup here.
+        showExistingPrefs()
     }
     
     @IBAction func okButtonClicked(_ sender: Any) {
+        saveNewPrefs()
+        view.window?.close()
     }
     
     @IBAction func cancelButtonClicked(_ sender: Any) {
+        view.window?.close()
     }
     
+    // 設定値でUIを更新する
+    func showExistingPrefs() {
+        let taskDurationInMinutes         = Int(prefs.taskDuration)         / 60
+        let intervalDurationInMinutes     = Int(prefs.intervalDuration)     / 60
+        let longIntervalDurationInMinutes = Int(prefs.longIntervalDuration) / 60
+        
+        taskDurationTextField.stringValue         = "\(taskDurationInMinutes)"
+        intervalDurationTextField.stringValue     = "\(intervalDurationInMinutes)"
+        longIntervalDurationTextField.stringValue = "\(longIntervalDurationInMinutes)"
+    }
+    
+    // 現在の設定値をNSUserDefaultsに保存し、通知を行う
+    func saveNewPrefs() {
+        prefs.taskDuration         = TimeInterval(taskDurationTextField.intValue         * 60)
+        prefs.intervalDuration     = TimeInterval(intervalDurationTextField.intValue     * 60)
+        prefs.longIntervalDuration = TimeInterval(longIntervalDurationTextField.intValue * 60)
+        
+        NotificationCenter.default.post(name: Notification.Name(rawValue: "PrefsChanged"),
+                                        object: nil)
+    }
 }
