c83f99871b24431ff495595d25037f8cac657438
IKEH <pondau1024@gmail.com>
2019-12-22 17:02:25 +0900

    WIndowの位置設定

--- 02_Windows and WindowController Tutorial for macOS/BabyScript/BabyScript/WindowController.swift
+++ 02_Windows and WindowController Tutorial for macOS/BabyScript/BabyScript/WindowController.swift
@@ -14,6 +14,19 @@ class WindowController: NSWindowController {
     super.windowDidLoad()
     
     // Implement this method to handle any initialization after your window controller's window has been loaded from its nib file.
+    //1.
+    if let window = window, let screen = window.screen {
+      let offsetFromLeftOfScreen: CGFloat = 100
+      let offsetFromTopOfScreen: CGFloat = 100
+      //2.
+      let screenRect = screen.visibleFrame
+      //3.
+      print("\(screenRect.maxY) - \(window.frame.height) - \(offsetFromTopOfScreen)")
+      let newOriginY = screenRect.maxY - window.frame.height - offsetFromTopOfScreen
+      print("\(newOriginY)")
+      //4.
+      window.setFrameOrigin(NSPoint(x: offsetFromLeftOfScreen, y: newOriginY))
+    }
   }
   
   required init?(coder: NSCoder) {
