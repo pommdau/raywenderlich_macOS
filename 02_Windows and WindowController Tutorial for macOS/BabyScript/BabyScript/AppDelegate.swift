2487f43bacf3d1e48d79ac5508f43474a067dbe7
IKEH <pondau1024@gmail.com>
2019-12-22 16:11:58 +0900

    create project

---
+++ 02_Windows and WindowController Tutorial for macOS/BabyScript/BabyScript/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  BabyScript
+//
+//  Created by Hiroki Ikeuchi on 2019/12/22.
+//  Copyright © 2019 ikeh1024. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+  func applicationDidFinishLaunching(_ aNotification: Notification) {
+    // Insert code here to initialize your application
+  }
+
+  func applicationWillTerminate(_ aNotification: Notification) {
+    // Insert code here to tear down your application
+  }
+
+
+}
+
---
+++ 02_Windows and WindowController Tutorial for macOS/BabyScript/BabyScript/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  BabyScript
+//
+//  Created by Hiroki Ikeuchi on 2019/12/22.
+//  Copyright © 2019 ikeh1024. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+
+    // Do any additional setup after loading the view.
+  }
+
+  override var representedObject: Any? {
+    didSet {
+    // Update the view, if already loaded.
+    }
+  }
+
+
+}
+
